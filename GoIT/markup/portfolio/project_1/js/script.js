8b50489a73abc28d419f4e488ecc92dba18be9fe
CasTroy <dops809@gmail.com>
2016-01-28 23:04:45 +0200

    pf

--- GoIT/markup/portfolio/project_1/js/script.js
+++ GoIT/markup/portfolio/project_1/js/script.js
@@ -1,4 +1,39 @@
 'use strict'
 $(function(){
+
 	
+
+
+showInformation('#educ', '.education');
+showInformation('#cource', '.courses');
+
+
+
+function showInformation(id, block){
+	var check = false;
+	var stopClick = true;
+	$(id).hover()
+
+	/*
+	$(id).on('click', function(){
+	if(stopClick)
+	{
+		stopClick = false;
+		if(!check){
+			$(block).animate({
+				height: "250px"
+			}, 500, function(){
+				stopClick = true;
+			})
+		}else{
+			$(block).animate({
+				height: "50px"
+			}, 500, function(){
+				stopClick = true;
+			})
+		}
+		check =!check;
+	}	
+	})*/
+}
 });
