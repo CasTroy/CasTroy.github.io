f623e01d1b5e186e436d4edfc9a37c150a391070
CasTroy <dops809@gmail.com>
2016-03-12 20:03:02 +0200

    add masondy and ajax queres

--- GoIT/markup/JavaScript/exam/src/js/script.js
+++ GoIT/markup/JavaScript/exam/src/js/script.js
@@ -3,8 +3,10 @@
 	document.getElementById("content").style.opacity = "0.5";
 	document.getElementById("content").style.filter = "progid:DXImageTransform.Microsoft.Alpha(opacity=50)";*/
 
+
 'use strict'
 $(function(){
+    //courusel
 	$('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
@@ -15,12 +17,45 @@ $(function(){
         }
     }
 	})
-
-    $('.grid').masonry({
-      // options...
-      itemSelector: '.grid-item',
-      columnWidth: 200
+    //masonry
+    $('.paint').masonry({
+      itemSelector: '.paint__item',
+      columnWidth: '.paint__item'
     });
-    
+
+
+    //picture
+    $.getJSON('http://api.pixplorer.co.uk/image?word=black background&amount=7&size=m', function(data){
+        var array = [];
+        var obj = {
+            url: 'none',
+            word: 'none'
+        }
+        for(var index in data.images)
+        {
+            obj.url = data.images[index].imageurl;
+            obj.word = data.images[index].word;
+            array.push(data.images[index]);
+        }
+        var model = new Model(array);
+        var view = new View(model);
+    }); 
 });
 
+
+//m
+function Model(data){
+    var self = this;
+    self.data = data;
+}
+//v
+function View(model){
+    var self = this;
+    self.renderList = function (data) {
+        var html = $('#template').html();
+        var content = tmpl(html, model);
+        $('.ideas').append(content)
+    };
+    self.renderList();
+}
+//c
