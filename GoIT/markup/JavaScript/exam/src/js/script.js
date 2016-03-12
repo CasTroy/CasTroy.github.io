a8881f1756e49d061e7dfe0ba1d26b8a3c18de7a
CasTroy <dops809@gmail.com>
2016-03-12 14:17:20 +0200

    add sliders

--- GoIT/markup/JavaScript/exam/src/js/script.js
+++ GoIT/markup/JavaScript/exam/src/js/script.js
@@ -3,3 +3,24 @@
 	document.getElementById("content").style.opacity = "0.5";
 	document.getElementById("content").style.filter = "progid:DXImageTransform.Microsoft.Alpha(opacity=50)";*/
 
+'use strict'
+$(function(){
+	$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        }
+    }
+	})
+
+    $('.grid').masonry({
+      // options...
+      itemSelector: '.grid-item',
+      columnWidth: 200
+    });
+    
+});
+
