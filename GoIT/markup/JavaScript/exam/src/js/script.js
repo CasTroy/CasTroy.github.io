cf31376cf312c348df079a2173afdaa1628e4936
CasTroy <dops809@gmail.com>
2016-03-17 16:35:56 +0200

    ajax ie8

--- GoIT/markup/JavaScript/exam/src/js/script.js
+++ GoIT/markup/JavaScript/exam/src/js/script.js
@@ -2,10 +2,8 @@
 	document.getElementById("content").style.color = "red";
 	document.getElementById("content").style.opacity = "0.5";
 	document.getElementById("content").style.filter = "progid:DXImageTransform.Microsoft.Alpha(opacity=50)";*/
- 
 
-
-
+   
 'use strict'
 $(function(){
     //courusel
@@ -19,7 +17,51 @@ $(function(){
             }
         }
 	})
-    //ajax images
+
+
+
+/*
+$.ajax({
+    url: 'https://api.pixplorer.co.uk/image?word=black bg&amount=7&size=m',
+    data: 'this is data being posted to the server',
+    contentType: 'text/plain',
+    type: 'POST',
+    dataType: 'json'
+    }).done(function(data) {
+       document.getElementById("lkn").setAttribute("href", "https://www.google.com.ua/?gws_rd=ssl"); 
+});*/
+
+if(window.XDomainRequest) {
+    document.getElementById("lkn").setAttribute("href", "https://www.google.com.ua/?gws_rd=ssl"); 
+    
+    // Use Microsoft XDR
+    var xdr = new XDomainRequest();
+    xdr.open("get", "http://api.pixplorer.co.uk/image?word=black bg&amount=7&size=m");
+   /* xdr.onload = function () {
+    var JSON = $.parseJSON(xdr.responseText);
+    if (JSON == null || typeof (JSON) == 'undefined')
+    {
+        JSON = $.parseJSON(data.firstChild.textContent);
+    }
+    processData(JSON);
+    };
+    xdr.onprogress = function(){ };
+    xdr.ontimeout = function(){ };
+    xdr.onerror = function () { };
+    setTimeout(function(){
+        xdr.send();
+    }, 0);*/
+} else {
+    alert(1)
+}
+
+
+
+
+
+
+
+  /*  //ajax images
     $.ajax({
         url: 'http://api.pixplorer.co.uk/image?word=black bg&amount=7&size=m',
         success: function(data){
@@ -27,7 +69,14 @@ $(function(){
             var view = new View(model);
             var controller = new Controller(model, view);
         }
+
+
+
     })
+*/
+    
+
+
     //hide butoon of navigations
         $('.owl-prev').html(' ');
         $('.owl-next').html(' ');
@@ -57,13 +106,12 @@ function Model(obj){
 function View(model){
     var self = this;
     self.renderList = function () {
-
-
         var wrapper = tmpl($('#template').html(), model);
         $('.ideas').append(wrapper)
-
-        $('#gallery').imagesLoaded(function (){
-           $('.grid').masonry({
+    
+        
+      $('#gallery').imagesLoaded(function (){
+            $('.grid').masonry({
               itemSelector: '.grid--item',
               columnWidth: '.grid--item',
               gutter: 10
@@ -87,4 +135,20 @@ function Controller(model, view){
             }
         })
     })
+}
+//parser
+function parser(str) {  
+  var rgxp = /([\S]*)\s?=\s?(\d*)/g,
+    result = {},
+    substr, keys, parents, i;
+  while (substr = rgxp.exec(str)) {
+    substr = substr[0].split("=")
+    keys = substr[0].trim().split(".")
+    parent = result
+    for (i = 0, ln = keys.length; i < ln; i++) {
+      parent[keys[i]] = parent[keys[i]] || {}
+      i == (ln - 1) ? (parent[keys[i]] = substr[1].trim()) : (parent = parent[keys[i]])
+    }
+  }
+  return result
 }
