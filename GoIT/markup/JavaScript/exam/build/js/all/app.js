d406bbf87c6eba164a2cb6efa5f0e9ff15e0f536
CasTroy <dops809@gmail.com>
2016-03-25 12:33:54 +0300

    add event for masonry ie

--- GoIT/markup/JavaScript/exam/build/js/all/app.js
+++ GoIT/markup/JavaScript/exam/build/js/all/app.js
@@ -77,10 +77,48 @@ require(
         $('.owl-prev').html(' ');
         $('.owl-next').html(' ');
 		
+		$('body').on('click', '.item--picture', show);
+		$('body').on('click', '.center', remove);
 });
 
 
+function show(){
 
+	$('body').append('<div class = "center"><img src = "#" class = "center__img"></div>')
 
+	$('html body').css('min-height', '100%')
 
+	$('.center').css({
+		'min-width': '100%',
+		'min-height': '100%',
+		background: 'rgba(0,0,0,0.8)',
+		'top': '0px',
+		'width': '100%', 
+		'position': 'fixed',
+		'z-index': '3'
+	})
+	$('.center__img').attr('src', $(this).attr('src'))
+	$('.center__img').css({
+		top: '50%', 
+		left: '50%',
+		width: '50%',
+		margin: '-25% 0 0 -25%',
+		'position': 'absolute',
+	})
 
+}
+
+function remove(){
+	$('.center').remove();
+}
+/*
+#center {
+	top: 50%; 
+	left: 50%; 
+	width: 450px; 
+	height: 450px; 
+	position: absolute; 
+	margin-top: -225px; 
+	margin-left: -225px; 
+}
+*/
