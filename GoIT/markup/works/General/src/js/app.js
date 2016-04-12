9d84f013ed7a1d89dbdf315919bd20a2ee479809
CasTroy <dops809@gmail.com>
2016-04-12 15:31:00 +0300

    AMD + AngularJS

--- GoIT/markup/works/General/src/js/app.js
+++ GoIT/markup/works/General/src/js/app.js
@@ -1,28 +1,3 @@
-requirejs.config({
-	paths: {
-		'jquery': 'jquery-1.12.2.min',
-		'imagesloaded' : 'imagesloaded-v3.min',
-		'masonry': 'masonry-v3.min'
-	},
-	shim:{
-		'jquery': {
-			exports: 'jQuery'
-		},
-		'imagesloaded': {
-			deps: ['jquery'],
-			exports: 'imagesloaded'
-		},
-		'masonry': {
-			exports: 'masonry'
-		}
-	}
-});
-require(
-	[
-		'jquery',
-		'imagesloaded',
-		'masonry',
-	],
-function ($, imagesloaded, masonry){
-	
-});
+define(['angular'], function(angular){
+	return angular.module('MyApp', []);
+})
---
+++ GoIT/markup/works/General/src/js/controllers/MainCtrl.js
@@ -0,0 +1,5 @@
+require(['app'], function(app){
+	app.controller('MainCtrl', function(){
+		this.message = "Hello world"
+	})
+})
